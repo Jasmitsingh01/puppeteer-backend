57812c64c4e7563dfd8cce881547ac0245bc23e6
Jasmit Singh <97171634+Jasmitsingh01@users.noreply.github.com>
2025-09-13 21:52:37 +0530

    refactor: update devtools proxy configuration and ports
    - Switch from puppeteer to puppeteer-extra for enhanced functionality
    - Update remote debugging port from 9223 to 9222
    - Modify nginx configuration to use dynamic host headers
    - Improve WebSocket URL rewriting for dynamic hosts

--- index-puppeteer-extra.js
+++ index-puppeteer-extra.js
@@ -1,10 +1,11 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
 const http = require('http');
 
 // Use stealth plugin
 puppeteer.use(StealthPlugin());
+
 (async () => {
     const browser = await puppeteer.launch({
         headless: false,
@@ -20,7 +21,7 @@ puppeteer.use(StealthPlugin());
             '--disable-backgrounding-occluded-windows',
             '--disable-renderer-backgrounding',
             '--start-maximized',
-            '--remote-debugging-port=9223',
+            '--remote-debugging-port=9222',
             '--remote-allow-origins=*',
             '--remote-debugging-address=0.0.0.0',
             '--disable-web-security',
@@ -34,7 +35,7 @@ puppeteer.use(StealthPlugin());
   const browserWSEndpoint = browser.wsEndpoint();
   fs.writeFileSync('wsEndpoint.txt', browserWSEndpoint);
 
-  http.get('http://localhost:9223/json/version', res => {
+  http.get('http://localhost:9222/json/version', res => {
     let data = '';
     res.on('data', chunk => (data += chunk));
     res.on('end', () => {
